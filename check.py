da7dc96f0f185340eb3a130c63b11603bc5bafc7
Crutas <renekauka@gmail.com>
2026-04-15 13:52:54 +0200

    Update check.py

--- check.py
+++ check.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import requests
 
-URL = os.getenv("https://store.steampowered.com/sale/steamdeckrefurbished/")
+URL = os.getenv("https://store.steampowered.com/app/2670660/Steam_Deck_OLED/")
 KEYWORD = "Nicht vorrätig"
 
 html = requests.get(URL).text
